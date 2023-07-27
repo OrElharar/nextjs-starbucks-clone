07e9487c08cbdf957b89f8e1cd614ced0a674c41
or <elhararor207@gmail.com>
2023-07-27 09:36:58 +0300

    Footer.tsx, social media dummy links: 1. Removed unwanted behavior of jumping to the top of the page while clicking on social media link. 2. Added the use of methodNotImplemented on clicking a social media.

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,21 +1,25 @@
-import React from "react";
+'use client';
+
+import React, {useContext} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import classes from "./Footer.module.scss";
 import messages from "@/public/messages.json";
+import {AlertContext} from "@/app/contexts/alert/AlertContext";
 
 const Footer: React.FC = () => {
+    const { methodNotImplemented } = useContext(AlertContext);
     return (
         <footer className={classes.footer}>
             <div className={classes.footerTitle}>{messages.footerTitle}</div>
-            <div className={classes.socialIcons}>
-                <a href="#" className={classes.socialIcon}>
+            <div className={classes.socialIcons} onClick={()=> methodNotImplemented()}>
+                <a className={classes.socialIcon}>
                     <FontAwesomeIcon icon={faFacebook} />
                 </a>
-                <a href="#" className={classes.socialIcon}>
+                <a className={classes.socialIcon}>
                     <FontAwesomeIcon icon={faTwitter} />
                 </a>
-                <a href="#" className={classes.socialIcon}>
+                <a className={classes.socialIcon}>
                     <FontAwesomeIcon icon={faInstagram} />
                 </a>
             </div>
